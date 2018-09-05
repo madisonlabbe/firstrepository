2f232a41cf0ca8aa87665e91e28bdb9df2a5250d
Madison Labbe <sMadisonLabbe@yccc.edu>
2018-09-05 19:09:09 -0400

    first commit

---
+++ src/myfirstproject/firstClass.java
@@ -0,0 +1,18 @@
+package myfirstproject;
+
+public class firstClass {
+
+	//An object is 2 things.
+	//1. Actions. Methods.
+	//2. Attributes. Variables.
+	private String name = "Maddie";
+	
+	// main method. Starts the program.
+	//Method signature - What makes this method unique. Defines the method.
+	public static void main(String[] args) {
+	//Prints to the console
+		System.out.println("firstClass");
+		System.out.println("helloWorld");
+		System.out.println("firstJAVA");
+	}
+}
